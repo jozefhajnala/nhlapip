4efa16289a0f27b5138e56b35e5fa2cb594ae87a
Jozef Hajnala <jozef.hajnala@gmail.com>
2020-09-18 15:16:59 +0000

    Add metadata endpoints

--- ci/pandas_showcase.py
+++ ci/pandas_showcase.py
@@ -1,5 +1,6 @@
 from nhlapip.nhl_player import Player
 from nhlapip.nhl_game import Game
+from nhlapip.nhl_md_endpoints import *
 import pandas as pd
 
 # Player Data
@@ -20,3 +21,18 @@ game.get_linescore()
 print(pd.json_normalize(game.linescore.data["periods"]))
 game.get_feed()
 print(pd.json_normalize(game.feed.data["liveData"]["plays"]["allPlays"]))
+
+# Game Types metadata
+game_types = GameTypesMd()
+game_types.get_data()
+print(pd.json_normalize(game_types.data))
+
+# Game Statuses metadata
+game_statues = GameStatusesMd()
+game_statues.get_data()
+print(pd.json_normalize(game_statues.data))
+
+# Play Types metadata
+play_types = PlayTypesMd()
+play_types.get_data()
+print(pd.json_normalize(play_types.data))
--- ci/python_package.sh
+++ ci/python_package.sh
@@ -4,5 +4,6 @@ pip install coverage
 python setup.py install --user
 python setup.py test
 
+python -m pytest
 python -m coverage run -m pytest
 python -m coverage report
